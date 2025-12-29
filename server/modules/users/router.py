f72c3326aef64a31d20bf882c2061dc19af89568
tienphuc <nguyentrantienphuc03082006@gmail.com>
2025-12-29 09:38:50 +0700

    src code users

--- server/modules/users/router.py
+++ server/modules/users/router.py
@@ -1,1 +1,39 @@
-# API /register, /login 
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from modules.users import schemas,  curd
+from core import security
+from database.session import get_db
+
+router = APIRouter()
+@router.post("/regsiter")
+def register ( user: schemas.UserCreate, db: Session = Depends (get_db)):
+    existing_user = curd.get_user_by_email (db, user.email)
+    if existing_user:
+        raise HTTPException (
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email đã được sử dụng"
+        )
+    new_user = curd.create_user (
+        db,
+        email = user.email,
+        full_name = user.full_name,
+        password = user.password
+    )
+    return ("Dăng ký thành công" , new_user)
+
+@router.post("/login")
+def login( user: schemas.UserLogin, db: Session = Depends (get_db)):
+    db_user = curd.get_user_by_email (db, user.email)
+    if not db_user or not security.verify_password (user.password, db_user.hashed_password):
+        raise HTTPException (
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Email hoặc mật khẩu không chính xác"
+        )
+    access_token = security.create_access_token (
+        data={"sub": db_user.email}
+    )
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
---
+++ server/modules/users/schemas.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, EmailStr
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+
+    class Config:
+        from_attributes = True

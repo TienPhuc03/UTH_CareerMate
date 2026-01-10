87d4c47478865d25ff8d11aea509bb62cf85edc5
tienphuc <nguyentrantienphuc03082006@gmail.com>
2026-01-11 00:20:47 +0700

    fix

--- server/main.py
+++ server/main.py
@@ -1,35 +1,4 @@
-# from fastapi import FastAPI
-# from modules.users import router as user_router
-# from database.base import test_connection, engine, Base
-# from fastapi.middleware.cors import CORSMiddleware
 
-
-# app = FastAPI(title="Career Mates")
-
-# app.include_router(
-#     user_router.router,
-#     prefix="/api/auth",
-#     tags=["Auth"]
-# )
-# test_connection()
-# Base.metadata.create_all(bind=engine) #tạo bảng 
-
-
-# @app.get("/")
-# def read_root():
-#     return {"message": "Welcome to Career Mates API!"}
-
-# # # Thêm đoạn này để FE (cổng 5500) gọi được BE (cổng 8000)
-# # app.add_middleware(
-# #     CORSMiddleware,
-# #     allow_origins=["*"], 
-# #     allow_credentials=True,
-# #     allow_methods=["*"],
-# #     allow_headers=["*"],
-# # )
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database.base import Base, engine, test_connection
--- server/modules/users/curd.py
+++ server/modules/users/curd.py
@@ -1,26 +1,4 @@
-# # hàm để lưu thông tin (đại học, chuyên ngành, gpa,...) của user vào database
-# # crud.py
-# from sqlalchemy.orm import Session
-# from modules.users import model
-# from core.security import get_password_hash
-# from pydantic import SecretStr
 
-
-# def get_user_by_email(db: Session, email: str):
-#     return db.query(model.User).filter(model.User.email == email).first()
-
-# def create_user (db: Session, email: str , full_name: str, password: str):
-#     hashed_password = get_password_hash(password)
-#     user = model.User(
-#         email=email,
-#         full_name=full_name,
-#         hashed_password=hashed_password
-#     )
-#     print("Creating user:", user)
-#     db.add(user)
-#     db.commit()
-#     db.refresh(user)
-#     return user
 from sqlalchemy.orm import Session
 from modules.users.model import User
 from core.security import get_password_hash

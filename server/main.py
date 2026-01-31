7962ebe50e2ff639a687f226a5b3309534e301f1
TienPhuc03 <nguyentrantienphuc03082006@gmail.com>
2026-01-31 18:49:07 +0700

    refactor: simplify CVAnalysisResultPage.html by removing commented-out code

--- server/main.py
+++ server/main.py
@@ -35,11 +35,10 @@ app = FastAPI(
     redoc_url="/redoc"
 )
 
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500",
-        "http://localhost:5500",
-        "http://localhost:3000",],
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -97,9 +96,6 @@ def root():
         "docs": "/docs",
         "health": "/health"
     }
-@app.get("/")
-def root():
-    return {"message": "Welcome to Career Mates API!", "docs": "/docs"}
 
 
 @app.get("/health")
